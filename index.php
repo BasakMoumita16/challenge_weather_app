88756a4b687ec78cf4d6b22fbf827f825939d28f
BasakMoumita16 <moumitaduttabasak@gmail.com>
2019-12-04 15:55:23 +0100

    changes made

--- index.php
+++ index.php
@@ -1,56 +1,51 @@
 
-<?php
-
-if (isset($_POST["submit"])){
-$city = $_POST["cityName"];
-$string = "http://api.openweathermap.org/data/2.5/weather?q=".$city."&units=metric&appid=a21c5451c8b02861f30734c35155745c";
-$data = json_decode(file_get_contents($string));
-
-};
-?> 
-
-
 <!DOCTYPE html>
 <html>
-<header>
 
 <head>
-                <title>Weather_App</title>
-                <meta name ="viewport" content=" width=device-width, initial-scale=1.0">
-				<meta charset="utf-8" />
-                <link rel="stylesheet" href="./css/style.css">
-            
-            </head>
-</header>
+			<title>Weather_App</title>
+			<meta name ="viewport" content=" width=device-width, initial-scale=1.0">
+			<meta charset="utf-8" />
+			<link rel="stylesheet" href="./css/style.css">
+</head>
 <body>
 
 
 <center>
-	<form method="Post" action="index.php">
-	<h1>Type City and Country</h1>
-	<br><p>For Example Delhi,in</p>
-		<input class= "int" type="text"name="cityName"placeholder="Search City">
-		<input class = "btn" type="submit" name="submit">
-	</form>
-	
+<form method="Post" action="index.php">
+<h1>Check Weather</h1>
+
+	<input class= "int" type="text"name="cityName"placeholder="Search City">
+	<input class = "btn" type="submit" name="submit">
+</form>
+
 </center>
 
- 
+	
+<?php
+		
+if (isset($_POST["submit"])){
+$city = $_POST["cityName"];
+$string = "http://api.openweathermap.org/data/2.5/forecast?q=".$city."&units=metric&appid=a21c5451c8b02861f30734c35155745c";
+$data = json_decode(file_get_contents($string));
+echo "<div class ='container'>";
+foreach ($data->list as $element) {
+	$time = $element->dt_txt;
+	$time_space = "$time ";
+	$hour = substr($time_space,11,2);
+	if($hour == 18){
+		echo "<div class= 'item'>";
+			
+echo"<center><h2> ".date('l', strtotime($element->dt_txt))."</h2></center>";	
+echo"<center><h2>Date ".$element->dt_txt."</h2></center>";			
+echo "<center><h2>Temparature ".$element->main->temp."°c</h2></center>";
+echo "<center><h2>Humidity ".$element->main->humidity."%</h2></center>";
+echo "<center><h2>Windspeed ".$element->wind->speed."mph </h2></center>";
+echo "</div>";
 
-
-<div class="container">
-        
-			<?php
-			if (isset($_POST["submit"])){
-            echo "<center><h2>Temparature " .$data->main->temp."°c</h2></center>";
-            echo "<center><h2>Cloud ". $data->clouds->all."%</h2></center>"; 
-			echo "<center><h2>Humidity ".$data->main->humidity."%</h2></center>";
-			echo "<center><h2>Windspeed ".$data->wind->speed."mph </h2></center>";
-			};
-            ?>
-            
-        </div> 
-
-
+};
+}
+};
+?>
 </body>
 </html>
